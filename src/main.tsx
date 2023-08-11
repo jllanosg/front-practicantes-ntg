6b82280ffc773c7946a85fdd32b455ff5699dab4
José Llanos <jllanosgg@gmail.com>
2023-08-11 11:28:44 -0400

    agrega router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import App from './App'
+import ErrorPage from './error-page'
+
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<App/>,
+    errorElement: <ErrorPage/>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
